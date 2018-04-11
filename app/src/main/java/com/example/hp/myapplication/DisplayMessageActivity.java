0a89d53a0e785b46a48ce4eb41f06242b917ca4d
HP <zamanapp@outlook.com>
2018-04-11 09:10:35 +0530

    hello

---
+++ app/src/main/java/com/example/hp/myapplication/DisplayMessageActivity.java
@@ -0,0 +1,17 @@
+package com.example.hp.myapplication;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by HP on 11-Apr-18.
+ */
+
+class DisplayMessageActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+    }
+}
